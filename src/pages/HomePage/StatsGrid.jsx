a201c666da26782780a82c110ff0a64f9269ca64
Nana Kwamina Ebbin <kwaminaebbin@gmail.com>
2025-10-22 12:33:59 +0000

    feat: refactor StatsGrid component to improve class name handling and clean up JSX structure

--- src/pages/HomePage/StatsGrid.jsx
+++ src/pages/HomePage/StatsGrid.jsx
@@ -1,17 +1,18 @@
 import Card from "../../components/Card/Card";
 import CardContent from "../../components/Card/CardContent";
+import { cn } from "../../utils/util";
 
 function StatsGrid({ stats }) {
   return (
     <section className="mb-8 grid grid-cols-1 gap-6 max-[1440px]:grid-cols-2 max-lg:grid-cols-2 max-sm:grid-cols-1 min-[1440px]:grid-cols-4">
       {stats.map((stat, index) => {
         return (
-          <Card key={index} >
-            <CardContent >
+          <Card key={index}>
+            <CardContent>
               <div className="text-muted-foreground text-lg font-medium">
                 {stat.title}
               </div>
-              <i className={`h-5 w-5 ${stat.color} ${stat.icon}`} />
+              <i className={cn("h-5 w-5", stat.color, stat.icon)} />
             </CardContent>
             <div className="flex flex-col gap-1">
               <p className="text-primary text-3xl">{stat.value}</p>
