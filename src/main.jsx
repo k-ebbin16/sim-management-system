171336d622fbc78992eab6e174a93edd0a93ba05
Nana Kwamina Ebbin <kwaminaebbin@gmail.com>
2025-10-22 13:23:40 +0000

    fix: correct import path for ThemeContextProvider to ensure proper component loading

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import AuthProvider from "./context/providers/AuthContextProvider";
 import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
-import ThemeProvider from "./context/Providers/ThemeContextProvider";
+import ThemeProvider from "./context/providers/ThemeContextProvider";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
