ab3a0971bea88c73518efe0ebfa93db2fef4e34d
Nana Kwamina Ebbin <kwaminaebbin@gmail.com>
2025-10-24 10:12:54 +0000

    Remove table component

--- src/components/Table/TableBody.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "../../utils/util";
-
-function TableBody({ className, ...props }) {
-  return (
-    <tbody className={cn("[&_tr:last-child]:border-0", className)} {...props} />
-  );
-}
-
-export default TableBody;
--- src/components/Table/TableHeader.jsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "../../utils/util";
-
-function TableHeader({ className, ...props }) {
-  return <thead className={cn("[&_tr]:border-b", className)} {...props} />;
-}
-
-export default TableHeader;
