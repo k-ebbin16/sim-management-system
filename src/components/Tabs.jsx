4eaac764a63bcc0485f1c4b5389cf9b673a36e6a
Nana Kwamina Ebbin <kwaminaebbin@gmail.com>
2025-10-31 15:51:40 +0000

    Ignore Linting error in Tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -18,6 +18,7 @@ function Tabs({
     }
   }, [value]);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleTabChange = (newValue) => {
     if (value === undefined) {
       setActiveTab(newValue);
